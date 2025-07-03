9d30c96304e8d1b4af7f4cda1fd6917a776d63b0
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-07-03 01:57:07 +0000

    Random Card Js Logic and Styles

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardValue = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+  let cardSuit = ['♦', '♥' ,'♠' ,'♣']
+
+  
+
+  function getRandomValue(Array){
+    return Array[Math.floor(Math.random()* cardSuit.length)]
+   }
+   let suitPrint = getRandomValue(cardSuit)
+
+    let upperSuit =  document.getElementById('upperSuit')
+   let bottomSuit =  document.getElementById('bottomSuit')
+
+   if (suitPrint == '♦' || suitPrint =='♥'){
+    upperSuit.style.color = 'red';
+    bottomSuit.style.color = 'red';
+
+   }
+
+
+  console.log(getRandomValue(cardValue),getRandomValue(cardSuit));
+  document.getElementById('cardNumber').textContent = getRandomValue(cardValue)
+    document.getElementById('upperSuit').textContent = suitPrint
+    document.getElementById('bottomSuit').textContent = suitPrint
+
+  
 };
